d849414d74fe681b8c26d0385c85842a927b98a7
Alexandra Maresh <maresh.al@yandex.ru>
2023-03-14 16:28:12 +0300

    7kyu kata

---
+++ codewars/7kyu kata/mumbling.py
@@ -0,0 +1,17 @@
+# This time no story, no theory. The examples below show you how to write function accum:
+#
+# Examples:
+# accum("abcd") -> "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") -> "C-Ww-Aaa-Tttt"
+# The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+def accum(s):
+    res= []
+    counter = 1
+    for i in s:
+        s= i * counter
+        res.append(s.capitalize())
+        counter +=1
+
+    return '-'.join(res)
